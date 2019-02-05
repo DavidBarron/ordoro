cff075f701435111eb2b5385f1fdcda6a4742d31
David Barrón <david.barron91@gmail.com>
2019-02-05 15:40:16 -0600

    Add files via upload

---
+++ david_barron_ordoro.py
@@ -0,0 +1,81 @@
+import requests
+from dateutil.parser import parse
+
+
+MY_EMAIL = 'david.barron91@gmail.com'
+ORDORO_API = 'https://us-central1-marcy-playground.cloudfunctions.net/ordoroCodingTest'
+
+
+def get_distinct_emails(data):
+    """
+    data is list of entry dicts with key "email"
+    return list of strings of unique emails
+    """
+    return list({e['email'] for e in data if e['email'] and '@' in e['email']})  # let set() handle uniqueness
+
+
+def get_domain_counts(data):
+    """
+    data is list of entry dicts with key "email"
+    return dict of domain string and int count
+    """
+    dom_count = dict()
+
+    for e in data:
+        if e['email'] and '@' in e['email']:  # lazy check valid email
+            domain = e['email'].split('@')[1]
+            if domain in dom_count:
+                dom_count[domain] += 1
+            else:
+                dom_count[domain] = 1
+
+    return dom_count
+
+
+def is_april_datetime(login_date):
+    """
+    login_date is datetime string with UTC offset
+    return boolean
+    """
+    date = parse(login_date) # error check would be nic
+    return date.month == 4
+
+
+def get_april_logins(data):
+    """
+    data is list of entry dicts with keys "email" and "login_date"
+    return list of strings of unique emails with datetime strings in the month of April
+    """
+    return list({e['email'] for e in data if e['email'] and e['login_date'] and is_april_datetime(e['login_date'])})
+
+
+def do_etl():
+    response = requests.get(ORDORO_API)
+
+    if response.status_code != 200:
+        print(f"Error making API call. Returned status code: {response.status_code}.")
+        return
+
+    body = response.json()
+
+    if not body:
+        print("No body in response.")
+        return
+
+    emails = get_distinct_emails(body['data'])  # could error check here too, but don't wanna...
+    domain_count = get_domain_counts(body['data'])
+    april_logins = get_april_logins(body['data'])
+
+    payload = {'your_email_address': MY_EMAIL, 'unique_emails': emails, 'user_domain_counts': domain_count,
+               'april_emails': april_logins}
+
+    post_response = requests.post(ORDORO_API, json=payload)
+
+    if post_response.status_code != 200:
+        print(f"Failed to post payload to Ordoro. Returned status code: {post_response.status_code}")
+    else:
+        print("Successfully posted payload to Ordoro.")
+
+
+if __name__ == "__main__":
+    do_etl()
---
+++ ordoro_test.py
@@ -0,0 +1,97 @@
+import unittest
+from david_barron_ordoro import get_distinct_emails, get_domain_counts, get_april_logins
+
+
+class TestOrdoro(unittest.TestCase):
+
+    def test_get_distinct_emails_pass(self):
+        given = [{"login_date": None, "email": 'blah1@test.com'}, {"login_date": None, "email": 'blah2@test.com'}]
+
+        result = get_distinct_emails(given)
+
+        self.assertCountEqual(result, ['blah2@test.com', 'blah1@test.com'])
+
+    def test_get_distinct_emails_none_pass(self):
+        given = [{"login_date": None, "email": 'blah1@test.com'}, {"login_date": None, "email": None}]
+
+        result = get_distinct_emails(given)
+
+        self.assertCountEqual(result, ['blah1@test.com'])
+
+    def test_get_distinct_emails_empty_pass(self):
+        given = [{"login_date": None, "email": 'blah1@test.com'}, {"login_date": None, "email": ''}]
+
+        result = get_distinct_emails(given)
+
+        self.assertCountEqual(result, ['blah1@test.com'])
+
+    def test_get_distinct_emails_duplicate_pass(self):
+        given = [{"login_date": None, "email": 'blah1@test.com'}, {"login_date": None, "email": 'blah1@test.com'}]
+
+        result = get_distinct_emails(given)
+
+        self.assertCountEqual(result, ['blah1@test.com'])
+
+    def test_get_domain_counts_1_pass(self):
+        given = [{"login_date": None, "email": 'blah1@test.com'}, {"login_date": None, "email": 'blah2@test.com'}]
+
+        result = get_domain_counts(given)
+
+        self.assertDictEqual(result, {'test.com': 2})
+
+    def test_get_domain_counts_2_pass(self):
+        given = [{"login_date": None, "email": 'blah1@test.com'}, {"login_date": None, "email": 'blah2@gmail.com'}]
+
+        result = get_domain_counts(given)
+
+        self.assertDictEqual(result, {'test.com': 1, 'gmail.com': 1})
+
+    def test_get_domain_counts_none_pass(self):
+        given = [{"login_date": None, "email": 'blah1@test.com'}, {"login_date": None, "email": None}]
+
+        result = get_domain_counts(given)
+
+        self.assertDictEqual(result, {'test.com': 1})
+
+    def test_get_domain_counts_not_email_pass(self):
+        given = [{"login_date": None, "email": 'blah1@test.com'}, {"login_date": None, "email": 'not an email, yo'}]
+
+        result = get_domain_counts(given)
+
+        self.assertDictEqual(result, {'test.com': 1})
+
+    def test_get_april_logins_1_pass(self):
+        given = [{"login_date": '2014-04-18T05:43:51+08:00', "email": 'blah1@test.com'},
+                 {"login_date": '2014-05-18T05:43:51+08:00', "email": 'blah2@test.com'}]
+
+        result = get_april_logins(given)
+
+        self.assertCountEqual(result, ['blah1@test.com'])
+
+    def test_get_april_logins_2_pass(self):
+        given = [{"login_date": '2014-04-18T05:43:51+08:00', "email": 'blah1@test.com'},
+                 {"login_date": '2014-04-05T05:43:51+08:00', "email": 'blah2@test.com'}]
+
+        result = get_april_logins(given)
+
+        self.assertCountEqual(result, ['blah1@test.com', 'blah2@test.com'])
+
+    def test_get_april_logins_none_login_pass(self):
+        given = [{"login_date": None, "email": 'blah1@test.com'},
+                 {"login_date": '2014-04-05T05:43:51+08:00', "email": 'blah2@test.com'}]
+
+        result = get_april_logins(given)
+
+        self.assertCountEqual(result, ['blah2@test.com'])
+
+    def test_get_april_logins_none_login_no_april_date_pass(self):
+        given = [{"login_date": None, "email": 'blah1@test.com'},
+                 {"login_date": '2014-07-05T05:43:51+08:00', "email": 'blah2@test.com'}]
+
+        result = get_april_logins(given)
+
+        self.assertCountEqual(result, [])
+
+
+if __name__ == '__main__':
+    unittest.main()
